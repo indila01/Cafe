069fe162b469f23a268157b9bc05e4ca5d1fd54e
Indila <Indiladineth@yahoo.com>
2024-09-09 04:50:12 +0530

    warn the user

--- Ui/src/components/modals/cafeModal.jsx
+++ Ui/src/components/modals/cafeModal.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Form, Input } from 'antd';
 
 const CafeModal = ({ isVisible, isEditMode, onCancel, onSubmit, initialValues }) => {
   const [form] = Form.useForm();
+  const [isFormChanged, setIsFormChanged] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (initialValues) {
       form.setFieldsValue(initialValues);
     } else {
@@ -12,24 +13,55 @@ const CafeModal = ({ isVisible, isEditMode, onCancel, onSubmit, initialValues })
     }
   }, [initialValues, form]);
 
+  useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      if (isFormChanged) {
+        e.preventDefault();
+        e.returnValue = '';
+      }
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [isFormChanged]);
+
+  const handleCancel = () => {
+    if (isFormChanged) {
+      const confirmClose = window.confirm("You have unsaved changes. Are you sure you want to close?");
+      if (confirmClose) {
+        onCancel();
+      }
+    } else {
+      onCancel();
+    }
+  };
+
   return (
     <Modal
       title={isEditMode ? "Edit Cafe" : "Add Cafe"}
       visible={isVisible}
-      onCancel={onCancel}
+      onCancel={handleCancel}
       onOk={() => {
         form
           .validateFields()
           .then(values => {
             form.resetFields();
             onSubmit(values);
+            setIsFormChanged(false);
           })
           .catch(info => {
             console.log('Validate Failed:', info);
           });
       }}
     >
-      <Form form={form} layout="vertical">
+      <Form
+        form={form}
+        layout="vertical"
+        onValuesChange={() => setIsFormChanged(true)}
+      >
         <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please input the name!' }]}>
           <Input />
         </Form.Item>
